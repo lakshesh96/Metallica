5a834c1eeece45633e087ad621420286372fd879
Lakshesh Girdhar <lakshesh.girdhar@sapient.com>
2018-04-10 19:09:20 +0530

    Class 4,5

---
+++ EquityTradingPlatformApi/EquityTradingPlatformApi/Models/Stocks.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EquityTradingPlatformApi.Models
+{
+    public class Stocks
+    {
+        [Required]
+        public int StockId { get; set; }
+        [Required]
+        public string Symbol { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public int CurrentPrice { get; set; }
+        [Required]
+        public int VolumeAvailable { get; set; }
+
+    }
+}
