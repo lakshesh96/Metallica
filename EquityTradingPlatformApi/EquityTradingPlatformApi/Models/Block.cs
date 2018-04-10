849fb5bf54262e5555573ea924a6db3c7265731e
Akul Rastogi <akurasto@publicisgroupe.net>
2018-04-10 18:39:00 +0530

    Classes 6,7

---
+++ EquityTradingPlatformApi/EquityTradingPlatformApi/Models/Block.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EquityTradingPlatformApi.Models
+{
+    public enum Status
+    {
+        Partial,
+        Pending,
+        Executed
+    }
+
+    public class Block
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public Status Status { get; set; }
+
+        public virtual User User { get; set; }
+
+        [Required]
+        public int UserId;
+    }
+}
