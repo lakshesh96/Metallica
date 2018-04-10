22ab43cd4ce0e32f043ab80d1eb037d465e4bac8
Akul Rastogi <akurasto@publicisgroupe.net>
2018-04-10 18:22:12 +0530

    Order Added

---
+++ EquityTradingPlatformApi/EquityTradingPlatformApi/Models/Order.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EquityTradingPlatformApi.Models
+{
+    public enum OrderType
+    {
+        Market,
+        Limit,
+        Stop,
+        StopLimit
+    }
+
+    public enum Side
+    {
+        Buy,
+        Sell
+    }
+    
+    public class Order
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public OrderType OrderType { get; set; }
+
+        [Required]
+        public Side OrderSide { get; set; }
+
+        [Required]
+        public string StockSymbol { get; set; }
+
+        [Required]
+        public int Quantity { get; set; }
+
+        public virtual User User { get; set; }
+
+        [Required]
+        public int UserId;
+
+    }
+}
