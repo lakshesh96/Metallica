e08efa9c81f0603bde8f949e34184b2f192fa2a6
Lakshesh Girdhar <lakshesh.girdhar@sapient.com>
2018-04-11 10:59:09 +0530

    Admin Controller

--- EquityTradingPlatformApi/EquityTradingPlatformApi/App_Start/WebApiConfig.cs
+++ EquityTradingPlatformApi/EquityTradingPlatformApi/App_Start/WebApiConfig.cs
@@ -19,6 +19,7 @@ namespace EquityTradingPlatformApi
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+            config.EnableCors();
         }
     }
 }
