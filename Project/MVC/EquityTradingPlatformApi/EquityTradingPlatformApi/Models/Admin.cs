fe34a42b344f1e929e6215b6bdbb90f2f986d97b
Ishan Jain <ishan.jain@sapient.com>
2018-04-11 11:26:36 +0530

    Revamping

---
+++ Project/MVC/EquityTradingPlatformApi/EquityTradingPlatformApi/Models/Admin.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EquityTradingPlatformApi.Models
+{
+    public class Admin
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+       
+    }
+}
---
+++ Project/MVC/EquityTradingPlatformApi/EquityTradingPlatformApi/Models/Stocks.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EquityTradingPlatformApi.Models
+{
+    public class Stocks
+    {
+        public int Id { get; set; } 
+
+        [Required]
+        public string Symbol { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public int CurrentPrice { get; set; }
+        [Required]
+        public int VolumeAvailable { get; set; }
+
+
+
+    }
+}
