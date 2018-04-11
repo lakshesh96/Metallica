2a4b42c65b1c0eae10939ea670d9a8e7c8a66cea
Ishan Jain <ishan.jain@sapient.com>
2018-04-11 11:15:06 +0530

    BYEBYE

--- EquityTradingPlatformApi/EquityTradingPlatformApi/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace EquityTradingPlatformApi.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
