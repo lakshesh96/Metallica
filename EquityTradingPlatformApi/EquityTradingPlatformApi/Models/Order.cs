2090e7b085e74dc98353f4a5937fd23cecbdf1d7
Ishan Jain <ishan.jain@sapient.com>
2018-04-11 10:42:16 +0530

    Stock Controller

--- EquityTradingPlatformApi/EquityTradingPlatformApi/Models/Order.cs
+++ EquityTradingPlatformApi/EquityTradingPlatformApi/Models/Order.cs
@@ -38,7 +38,7 @@ namespace EquityTradingPlatformApi.Models
         public Side OrderSide { get; set; }
 
         [Required]
-        public string StockId { get; set; }
+        public int StockId { get; set; }
 
         [Required]
         public int Quantity { get; set; }
