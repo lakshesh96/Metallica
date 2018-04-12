c0c8a201bbb3d2978e58f7b1b0d492eb7ffff97b
Lakshesh Girdhar <lakshesh.girdhar@sapient.com>
2018-04-12 10:54:04 +0530

    Ishan Gaandu (angular)

--- Project/Angular/EquityTradingSimulationFrontEnd/src/app/Services/stocks.service.ts
+++ Project/Angular/EquityTradingSimulationFrontEnd/src/app/Services/stocks.service.ts
@@ -1,21 +1,22 @@
 import { Injectable } from '@angular/core';
 import{Stocks} from '../Models/stocks';
+import{GlobalService} from '../Services/global.service';
 
 @Injectable()
 export class StocksService
 {
   StocksList:Stocks[]=[];
   
-  constructor()
+  constructor(private globalService:GlobalService)
   {
-    this.StocksList=
-    [
-      new Stocks("Infosys","Infosys",991),
-      new Stocks("Tata Consultancy Services","TCS",3058),
-      new Stocks("Reliance Industries","RIL",1465),
-      new Stocks("Punjab National Bank","PNB",99),
-      new Stocks("State Bank of India","SBI",254)
-    ]
+    // this.StocksList=
+    // [
+    //   new Stocks("Infosys","Infosys",991),
+    //   new Stocks("Tata Consultancy Services","TCS",3058),
+    //   new Stocks("Reliance Industries","RIL",1465),
+    //   new Stocks("Punjab National Bank","PNB",99),
+    //   new Stocks("State Bank of India","SBI",254)
+    // ]
   }
   Search(name)
   {
