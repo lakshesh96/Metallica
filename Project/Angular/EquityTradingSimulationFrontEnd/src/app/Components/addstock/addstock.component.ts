789200de6128cb68db57bd8294b873233f77e97c
Pankaj Garg <pangarg1@publicisgroupe.net>
2018-04-13 10:09:26 +0530

    addstock

---
+++ Project/Angular/EquityTradingSimulationFrontEnd/src/app/Components/addstock/addstock.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-addstock',
+  templateUrl: './addstock.component.html',
+  styleUrls: ['./addstock.component.css']
+})
+export class AddstockComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
