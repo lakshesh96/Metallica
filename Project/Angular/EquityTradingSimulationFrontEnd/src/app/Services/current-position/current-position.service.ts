283ae1bb476f7746f5d41a2ff1f1569037235618
Akul Rastogi <AkulRastogi@Akuls-MacBookPro.local>
2018-04-26 23:46:55 +0530

    Current Positions UI update

--- Project/Angular/EquityTradingSimulationFrontEnd/src/app/Services/current-position/current-position.service.ts
+++ Project/Angular/EquityTradingSimulationFrontEnd/src/app/Services/current-position/current-position.service.ts
@@ -1,47 +1,21 @@
 import { Injectable } from '@angular/core';
-import{CurrentPosition} from "../../Models/current-position"
-import {GlobalService} from '../../Services/global.service';
-import{Stocks} from "../../Models/stocks"
+import { CurrentPosition } from "../../Models/current-position"
+import { GlobalService } from '../../Services/global.service';
+import { Stocks } from "../../Models/stocks"
 
 @Injectable()
 export class CurrentPositionService {
 
-  CurrentS : CurrentPosition[];
+	CurrentS : CurrentPosition[];
 
-  //url = "http://localhost:52705/api/Position/Approved?userId=";
-  url = "api/Position/Approved?userId=";
- 
-  constructor(private globalService:GlobalService) {
+	//url = "http://localhost:52705/api/Position/Approved?userId=";
+	url = "api/Position/Approved?userId=";
 
-  this.GetPosition();
-  }
+	constructor(private globalService: GlobalService) { }
 
-  GetPosition(){
-    let userid = sessionStorage.getItem("UserId");
-    /* this.globalService.GetWithId(this.url,userid).subscribe(
-      response => this.CurrentS = response,
-      error => console.error(error),
-      () => console.log(this.CurrentS)
-    ); */
-    console.log(this.url);
-    console.log(userid);
-    return this.globalService.GetMethod(this.url+userid);
-  }
-
-/*   GetOrderDetails(id: number) {
-    this.globalService.GetWithId(this.urlOrder, id).subscribe(
-      response => {return response;},
-      error => console.error(error),
-      () => console.log(this.CurrentS)
-    );
-
-  }
-  GetStockDetails(id: number) {
-    this.globalService.GetWithId(this.urlStocks, id).subscribe(
-      response => {return response;},
-      error => console.error(error),
-      () => console.log(this.CurrentS)
-    );
-  } */
-
+	GetPosition(){
+		let userId = sessionStorage.getItem("UserId");
+		console.log("Current Position for UserId:", userId, "| API:", this.url);
+		return this.globalService.GetMethod(this.url + userId);
+	}
 }
