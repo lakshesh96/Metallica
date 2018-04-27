d4345183d83f63416d1db8460bdacd3e6f4f5bd6
Akul Rastogi <akurasto@publicisgroupe.net>
2018-04-27 15:45:47 +0530

    Indentation improved

--- Metallica-Angular/src/app/app.module.ts
+++ Metallica-Angular/src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
+// Import Components
 import { AppComponent } from './app.component';
 
+// Import Services
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent
+	],
+	imports: [
+		BrowserModule
+	],
+	providers: [],
+	bootstrap: [
+		AppComponent
+	]
 })
 export class AppModule { }
