4f50d981a12b77174d362e059442204ce929ad44
Shivani Chandak <shichand2@publicisgroupe.net>
2018-04-30 15:55:08 +0530

    login

--- Metallica-Angular/src/app/app.module.ts
+++ Metallica-Angular/src/app/app.module.ts
@@ -1,18 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {
+  SocialLoginModule,
+  AuthServiceConfig,
+  GoogleLoginProvider
+} from "angular5-social-login";
 
 
 import { AppComponent } from './app.component';
-
+import { LoginComponent } from './Components/login/login.component';
+export function getAuthServiceConfigs() {
+  let config = new AuthServiceConfig(
+      [
+        {
+          id: GoogleLoginProvider.PROVIDER_ID,
+          provider: new GoogleLoginProvider("351844398119-1snsmgp63bgma6vo71sj5lgfskl48f64")
+        }
+      ]
+    )
+  return config;
+}
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SocialLoginModule
   ],
-  providers: [],
+  providers: [{provide: AuthServiceConfig,
+    useFactory: getAuthServiceConfigs}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
