ed0dc72db90b3af3b69b486d6575ece6621fbce3
Ishan Jain <ishan.jain@sapient.com>
2018-04-30 16:47:16 +0530

    MessageQueue for Commodity tickers,Trade added

---
+++ Metallica_API/Metallica/MQueue/TradeMQueue.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Messaging;
+using Metallica.Models;
+
+namespace Metallica.MQueue
+{
+    public class TradeMQueue
+    {
+        private string TradeQueueName = @".\Private$\Trade";
+        private Boolean SendMessage(Trade trade)
+            {
+                MessageQueue messageQueue = null;
+                if (!MessageQueue.Exists(TradeQueueName))
+                    messageQueue = MessageQueue.Create(TradeQueueName);
+                else
+                    messageQueue = new MessageQueue(TradeQueueName);
+                try
+                {
+                    messageQueue.Formatter = new XmlMessageFormatter(new Type[] { typeof(Trade) });
+                    messageQueue.Send(trade);
+                }
+                catch
+                {
+                    return false;
+                }
+                finally
+                {
+                    messageQueue.Close();
+                }
+            return true;
+            }
+
+
+            private Trade ReceiveMessage()
+            {
+                if (!MessageQueue.Exists(TradeQueueName))
+                    return null;
+                MessageQueue messageQueue = new MessageQueue(TradeQueueName);
+                Trade trade = null;
+                try
+                {
+                    messageQueue.Formatter = new XmlMessageFormatter(new Type[] { typeof(Trade) });
+                    trade = (Trade)messageQueue.Receive().Body;
+                }
+                catch { }
+                finally
+                {
+                    messageQueue.Close();
+                }
+                return trade;
+            }
+    }
+}
