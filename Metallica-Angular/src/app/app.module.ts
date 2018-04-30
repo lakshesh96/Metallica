dd7be9efbddf0caa5a7f340d832fa16bae34663b
Amrit Kharbanda <amrkharb@publicisgroupe.net>
2018-04-30 15:51:18 +0530

    Amrit Init

--- Metallica-Angular/src/app/app.module.ts
+++ Metallica-Angular/src/app/app.module.ts
@@ -3,12 +3,18 @@ import { NgModule } from '@angular/core';
 
 // Import Components
 import { AppComponent } from './app.component';
+import { AddTradeComponent } from './components/add-trade/add-trade.component';
+import { EditTradeComponent } from './components/edit-trade/edit-trade.component';
+import { TradeDetailsComponent } from './components/trade-details/trade-details.component';
 
 // Import Services
 
 @NgModule({
 	declarations: [
-		AppComponent
+		AppComponent,
+		AddTradeComponent,
+		EditTradeComponent,
+		TradeDetailsComponent
 	],
 	imports: [
 		BrowserModule
---
+++ Metallica-Angular/src/app/components/edit-trade/edit-trade.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-trade',
+  templateUrl: './edit-trade.component.html',
+  styleUrls: ['./edit-trade.component.css']
+})
+export class EditTradeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
