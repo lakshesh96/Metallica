060c47572ab96606ad8a72f33c960ee27db1a538
Lakshesh Girdhar <lakshesh.girdhar@sapient.com>
2018-05-01 10:42:41 +0530

    Login Error Fixed

--- Metallica-Angular/src/app/Components/login-oauth/login-oauth.component.ts
+++ Metallica-Angular/src/app/Components/login-oauth/login-oauth.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Login} from '../../Models/login';
+import { FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-login-oauth',
@@ -7,9 +9,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginOauthComponent implements OnInit {
 
+  login:FormGroup;
+	model: any = {};
+	loading = false;
+  returnUrl: string;
+  
+	url:string = "api/OAuth";
+
+	id:number;
+	UserId = null;
+	x:boolean =true;
+
   constructor() { }
 
   ngOnInit() {
+    this.login = new FormGroup({
+			Type: new FormControl('', [Validators.required]),
+			UserName: new FormControl('', [Validators.required]),
+			Password: new FormControl('', [Validators.required])
+		});
+		//this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+		this.UserId = sessionStorage.getItem("UserId");
+
+		if(this.UserId) {
+			this.x=false;
+			//this.navigate(sessionStorage.getItem("Type"));
+		}
+		else
+			this.x = true;
   }
+
+  onSubmit({ value, valid }: { value: Login, valid: boolean }) {
+    
+	}
 
 }
