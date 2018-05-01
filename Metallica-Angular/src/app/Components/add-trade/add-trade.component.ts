d1af4537b7564fb3ce0939b41f75937323a64f68
Amrit Kharbanda <amrkharb@publicisgroupe.net>
2018-05-01 13:07:02 +0530

    service

--- Metallica-Angular/src/app/Components/add-trade/add-trade.component.ts
+++ Metallica-Angular/src/app/Components/add-trade/add-trade.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component , OnInit } from '@angular/core';
+
+import {TradeForm} from '../../models/trade-form';
+import {TradeOperationService} from '../../Services/TradeOperation/trade-operation-service.service';
 
 @Component({
   selector: 'app-add-trade',
   templateUrl: './add-trade.component.html',
   styleUrls: ['./add-trade.component.css']
 })
-export class AddTradeComponent {
+export class AddTradeComponent implements OnInit {
 
-  constructor() { }
+  TradeList : TradeForm[];
+
+  constructor(private DS : TradeOperationService) { }
+  ngOnInit() {
+	  this.TradeList = this.DS.TradeList;
+  }
+
+  Add(date,commodity,side,counterparty,price,quantity,location){
+    var p = {TradeDate: date, Commodity: commodity, Side: side, Counterparty: counterparty, Price: price, Quantity:quantity, Location: location}
+	  this.DS.insert(p);
+  }
 }
