3e1d69ce49fd989102f3c8353e210bc027939824
Shivani Chandak <shichand2@publicisgroupe.net>
2018-05-01 13:05:56 +0530

    addtrade

--- Metallica-Angular/src/app/Components/add-trade/add-trade.component.ts
+++ Metallica-Angular/src/app/Components/add-trade/add-trade.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators} from '@angular/forms';
+import { TradeForm } from '../../Models/trade-form';
 
 @Component({
   selector: 'app-add-trade',
@@ -6,6 +8,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-trade.component.css']
 })
 export class AddTradeComponent {
-
+  TradeForm : FormGroup;
   constructor() { }
+  ngOnInit() {
+    this.TradeForm = new FormGroup({
+      Date: new FormControl(),
+      Commodity:new FormControl('', [Validators.required]),
+      Side:new FormControl(),
+      Counterparty:new FormControl('', [Validators.required]),
+      Price:new FormControl(),
+      Quantity:new FormControl('', [Validators.required]),
+      Location:new FormControl('', [Validators.required])
+  });
+  }
+  onSubmit({ value, valid }: { value: TradeForm, valid: boolean }) {
+    console.log(value, valid);
+  }
 }
