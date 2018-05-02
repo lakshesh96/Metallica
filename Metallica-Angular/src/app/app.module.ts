18b3d0b46d833eb4dee165fcb03011baf10d5079
Shivani Chandak <shichand2@publicisgroupe.net>
2018-05-02 15:38:43 +0530

    registerservice

--- Metallica-Angular/src/app/app.module.ts
+++ Metallica-Angular/src/app/app.module.ts
@@ -39,9 +39,6 @@ import { ReferenceDataService } from './Services/ReferenceData/reference-data.se
 import { SearchComponent } from './Components/search/search.component';
 import { MainComponent } from './Components/main/main.component';
 import { RoutesModule } from './Routes/RouteModule';
-import {AuthGuardService} from './Services/AuthGuard/auth-guard.service';
-import { SearchService } from './Services/Search/search.service';
-import { RegisterComponent } from './Components/register/register.component';
 
 @NgModule({
 	declarations: [
@@ -77,8 +74,7 @@ import { RegisterComponent } from './Components/register/register.component';
 		TradeTableService,
 		PriceTickerService,
 		ReferenceDataService,
-		SearchService,
-		AuthGuardService
+		SearchService
 	],
 	bootstrap: [
 		AppComponent
